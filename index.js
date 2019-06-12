9d90e9204e65867bbed2e34406a7e4ade57eef88
JonoStarsmore <46201874+JonoStarsmore@users.noreply.github.com>
2019-06-12 14:16:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+function myFunction() {
+  let x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
 
